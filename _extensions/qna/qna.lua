38205c7e0b10ae767a36706dce8884e0eace7194
Jon Reades <jon@reades.com>
2024-05-22 14:29:55 +0100

    So close... but so far away.

--- _extensions/qna/qna.lua
+++ _extensions/qna/qna.lua
@@ -1,25 +1,54 @@
 print("Loaded!")
 
-local function tabset(node)
-  print("Tabset")
-  return quarto.Callout({
-              type    = "info",
-              title   = "Foo",
-              content = "^^^^",
-              -- icon    = icon,
-          })
+-- Detects the Tabset (yay) but I can't
+-- tell if it's one of my 'special' Q&A tabsets
+-- or a 'normal' one.
+function Tabset(el)
+  print(">>>>>>>")
+  print("Tabset detected")
+
+  print("vvvvvvv")
+  for k, v in pairs(el) do
+    print('k: ' .. k)
+  end
+  print("^^^^^^^^")
+
+  -- el.attr seems to have the class information
+  -- but its a userdata type that I can't get useable
+  -- information from
+  print(el.attr)
+  print(el.attr[1]) -- was hoping for ['qna','panel-tabset']
+  print(type(el.attr)) -- but it's type userdata
+  print(getmetatable(el.attr)) -- this doesn't help
+  print('----')
+
+  -- and this seems to have no obvious order that relates
+  -- the source QMD file though there must be *some* link
+  print(el.__quarto_custom_node.content[1]) -- this is tab content
+  print(el.__quarto_custom_node.content[2]) -- this is tab title
+  print("<<<<<<<<")
 end
 
+-- Detects the Div (yay) but I can't 
+-- work out how to 'promote' the element
+-- to a tabset if I *don't* want to manipulate it
 function Div(el)
   if el.classes:includes"qna" then
-    print(el.classes)
-    if quarto.doc.is_format("html") then
-      el.classes[1] = 'panel-tabset'
-      return quarto.Tab(pandoc.Div('Test'), pandoc.Span('Title'))
-      -- return pandoc.SmallCaps(el.content)
-    else 
+    print(">>>>>>>")
+    print("Q&A detected")
+    if quarto.doc.is_format("html") then 
+      -- need to create tabset here
+      -- return quarto.Tabset(???)
+      print(el.classes)
+      el.classes[1] = 'panel-tabset' 
+      print(el.classes)
+      return el -- no effect on output
+
+    else -- Stub for PDF/IPYNB logic later
+      print("Not html")
       return nil
     end
+    print("<<<<<<<<")
   end
 end
 
