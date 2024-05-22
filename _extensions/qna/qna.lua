d8606b15ac9fff0e1dfb1b6b0d9cfd5798a6a7f9
Jon Reades <jon@reades.com>
2024-05-22 22:37:47 +0100

    Have function to generate panel tabsets. *Now* need to change action based on output type...

--- _extensions/qna/qna.lua
+++ _extensions/qna/qna.lua
@@ -1,88 +1,72 @@
-print("Loaded!")
-
--- Detects the Tabset (yay) but I can't
--- tell if it's one of my 'special' Q&A tabsets
--- or a 'normal' one.
-function Tabset(el)
-  print(">>>>>>>")
-  print("Tabset detected")
-
-  print("vvvvvvv")
-  for k, v in pairs(el) do
-    print('k: ' .. k)
+function Div(div)
+  if not div.classes:includes("qna") then 
+    return
   end
-  print("^^^^^^^^")
 
-  -- el.attr seems to have the class information
-  -- but its a userdata type that I can't get useable
-  -- information from
-  print(el.attr)
-  print(el.attr[1]) -- was hoping for ['qna','panel-tabset']
-  print(type(el.attr)) -- but it's type userdata
-  print(getmetatable(el.attr)) -- this doesn't help
-  print('----')
+  raw = {}
+  local div_num = 0
+  local header_level = 0
 
-  -- and this seems to have no obvious order that relates
-  -- the source QMD file though there must be *some* link
-  print(el.__quarto_custom_node.content[1]) -- this is tab content
-  print(el.__quarto_custom_node.content[2]) -- this is tab title
-  print("<<<<<<<<")
-end
+  div:walk({
+      Block = function(block)
+        print('vvvv')
+        print("Unknown block of type " .. block.t)
+        if next(block.classes) ~= nil then
+          print("Has classes: ")
+          print(block.classes)
+        end 
+        print("Has attributes: ")
+        for k,v in pairs(block.attributes) do
+          print(k)
+          print(v)
+        end
+        print('^^^^')
+      end,
+      Div = function(div)
+        if next(div.classes) ~= nil and (div.classes:includes('cell') or div.classes:includes('cell-output')) then
+          return {}
+        else
+          table.insert(raw[div_num].content, div)
+        end
+      end,
+      CodeBlock = function(code)
+        table.insert(raw[div_num].content, code)
+      end,
+      Para = function(para)
+        table.insert(raw[div_num].content, para)
+      end,
+      BulletList = function(bl)
+        table.insert(raw[div_num].content, bl)
+      end,
+      OrderedList = function(ol)
+        table.insert(raw[div_num].content, ol)
+      end,
+      Plain = function(pl)
+        return pl -- table.insert(raw[div_num].content, pl)
+      end,
+      Header = function(header)
+        if header_level == 0 then
+          header_level = header.level
+        end
+        if header.level == header_level then
+          div_num = div_num + 1
+          raw[div_num] = {}
+          raw[div_num]['title'] = header.content
+          raw[div_num]['content'] = {}
+        else
+          table.insert(raw[div_num].content, header)
+        end
+      end
+    })
 
--- Detects the Div (yay) but I can't 
--- work out how to 'promote' the element
--- to a tabset if I *don't* want to manipulate it
-function Div(el)
-  if el.classes:includes"qna" then
-    print(">>>>>>>")
-    print("Q&A detected")
-    if quarto.doc.is_format("html") then 
-      -- need to create tabset here
-      -- return quarto.Tabset(???)
-      print(el.classes)
-      el.classes[1] = 'panel-tabset' 
-      print(el.classes)
-      return el -- no effect on output
-
-    else -- Stub for PDF/IPYNB logic later
-      print("Not html")
-      return nil
-    end
-    print("<<<<<<<<")
+  tabs = {}
+  for i, t in pairs(raw) do
+    table.insert(tabs, quarto.Tab({title=t.title, content=t.content}) )
   end
+
+  return quarto.Tabset({
+    level = 3,
+    tabs = pandoc.List( tabs ),
+    attr = pandoc.Attr("", {"panel-tabset"}) -- this shouldn't be necessary but it's the bug I mentioned.
+  })
 end
-
--- function Div(div)
---   -- Set defaults
---   local title = nil
---   local ctype = nil
-
---   -- Set callout type
---   if div.classes[1] ~= nil and string.find(div.classes[1], "casa-") then
---       ctype = string.sub(div.classes[1], 6)
---   else 
---       ctype = ""
---   end
-
---   -- Look for a header
---   if div.content[1].tag == 'Header' then
---       title = div.content[1].content
---       table.remove(div.content,1)
---   end
-
---   -- Is the output for iPynb or something else?
---   if _G._quarto.format.isIpynbOutput() then
---       return calloutIpynb(div, ctype, title, div.content)
---   else
---       if ctype == "connection" then 
---           ctype = "note"
---       end
-
---       return quarto.Callout({
---           type    = ctype,
---           title   = title,
---           content = div.content,
---           -- icon    = icon,
---       })
---   end
--- end
